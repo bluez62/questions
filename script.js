3cd155cf929871bec4ef8425206d46103db54660
bluez62 <nicholaspaulrodriguez13@gmail.com>
2026-06-14 17:13:36 -0500

    Testing CSS

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const submitButton = document.getElementById("submit-button");
+const afterSubmit = document.querySelectorAll('after-submit');
 
 submitButton.addEventListener("click", function() {
-    alert("Thank you for submitting your answers!")
+    alert("Thank you for submitting your answers!");
+    afterSubmit.forEach(element => {
+        afterSubmit.style.visibility = 'hidden';
+    });
 });
